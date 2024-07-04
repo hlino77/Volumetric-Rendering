e32d664c2b80801e4ddd096b9d3950fa18f91362
inbum <inbum9634@gmail.com>
2024-07-04 11:24:03 +0900

    Fix : Renderer 수정중

--- Renderer/Reference/Headers/Engine_Defines.h
+++ Renderer/Reference/Headers/Engine_Defines.h
@@ -42,20 +42,20 @@ using namespace DirectX;
 #include "Engine_Struct.h"
 #include "Engine_Function.h"
 
-#ifdef _DEBUG
+//#ifdef _DEBUG
+//
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
+//
+//#ifndef DBG_NEW 
+//
+//#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
+//#define new DBG_NEW 
+//
+//#endif
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
-#ifndef DBG_NEW 
-
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
-#define new DBG_NEW 
-
-#endif
-
-#endif // _DEBUG
+//#endif // _DEBUG
 
 
 using namespace Engine;
