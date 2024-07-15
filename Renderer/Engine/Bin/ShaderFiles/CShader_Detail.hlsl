f052af63f82a7c05308567d97b70ecc919492e2d
inbum <inbum9634@gmail.com>
2024-07-15 16:55:30 +0900

    Fix : Noise Texture 수정

--- Renderer/Engine/Bin/ShaderFiles/CShader_Detail.hlsl
+++ Renderer/Engine/Bin/ShaderFiles/CShader_Detail.hlsl
@@ -12,7 +12,8 @@ void CSMain(uint3 DTid : SV_DispatchThreadID)
 {
     
     float3 vUV = float3(DTid.x / 32.f, DTid.y / 32.f, DTid.z / 32.f);
-    
+    vUV += float3(1.0f, 1.0f, 1.0f);
+
     float fFreq = 8.0f;
     
     float fWorley1 = worleyFbm(vUV, fFreq);
--- Renderer/Engine/Bin/ShaderFiles/CShader_Shape.hlsl
+++ Renderer/Engine/Bin/ShaderFiles/CShader_Shape.hlsl
@@ -10,10 +10,10 @@ RWTexture3D<float4> OutputTexture;
 [numthreads(8, 8, 8)]
 void CSMain(uint3 DTid : SV_DispatchThreadID)
 {
-    
     float3 vUV = float3(DTid.x / 128.f, DTid.y / 128.f, DTid.z / 128.f);
+    vUV += float3(1.0f, 1.0f, 1.0f);
     
-    float fFreq = 4.0f;
+    float fFreq = 4.0f; 
     
     float fPfbm = lerp(1.0f, perlinfbm(vUV, 4.0f, 7), 0.5f);
     fPfbm = abs(fPfbm * 2.0f - 1.0f); // billowy perlin noise
@@ -24,6 +24,11 @@ void CSMain(uint3 DTid : SV_DispatchThreadID)
     float fPerlinWorley = remap(fPfbm, 0.0f, 1.0f, fWorley1, 1.0f); // perlin-worley
    
     OutputTexture[DTid] = float4(fPerlinWorley, fWorley1, fWorley2, fWorley3);
+
+    //if (vUV.x == 0.0f)
+    //{
+        //OutputTexture[DTid] = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    //}
 }
 
 
